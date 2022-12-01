8a37c806e948503d825b89309bf8d3e405631079
parthcsun <parth.savaj.356@my.csun.edu>
2022-11-30 18:36:43 -0800

    add admin bool in emplyoyee

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace hotel_management
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
@@ -37,9 +40,14 @@ namespace hotel_management
         }
 
         // POST api/<EmployeesController>
-        [HttpPost]
-        public ActionResult<Employee> Post([FromBody] Employee employee)
+        [HttpPost("{IsAdmin}")]
+        public ActionResult<Employee> Post(bool IsAdmin, [FromBody] Employee employee)
         {
+            if (IsAdmin == false)
+            {
+                return NotFound("You are Not authorized to add employee");
+            }
+
             Employee tempUser = new Employee()
             {
                 Name = employee.Name,
@@ -51,17 +59,25 @@ namespace hotel_management
                 Phone=employee.Phone,
                 Email=employee.Email,
                 Address=employee.Address,
-                IsActive=employee.IsActive
+                IsActive=employee.IsActive,
+                IsAdmin=employee.IsAdmin
             };
 
             var savedEmployee = employeeService.Create(tempUser);
             return CreatedAtAction(nameof(Get), new { id = savedEmployee.Id }, savedEmployee);
         }
 
+       
+
         // PUT api/<EmployeesController>/5
-        [HttpPut("{id}")]
-        public ActionResult Put(string id, [FromBody] Employee employee)
+        [HttpPut("{id,IsAdmin}")]
+        public ActionResult Put(string id,bool IsAdmin, [FromBody] Employee employee)
         {
+            if (IsAdmin == false)
+            {
+                return NotFound("You are Not authorized to update employee");
+            }
+
             var existingEmployee = employeeService.Get(id);
 
             if (existingEmployee == null)
@@ -75,9 +91,14 @@ namespace hotel_management
         }
 
         // DELETE api/<EmployeesController>/5
-        [HttpDelete("{id}")]
-        public ActionResult Delete(string id)
+        [HttpDelete("{id,IsAdmin}")]
+        public ActionResult Delete(string id, bool IsAdmin)
         {
+            if (IsAdmin == false)
+            {
+                return NotFound("You are Not authorized to delete employee");
+            }
+
             var employee = employeeService.Get(id);
 
             if (employee == null)
@@ -90,35 +111,6 @@ namespace hotel_management
             return Ok($"Employee with Id = {id} deleted");
         }
 
-        //login api/<EmployeesController>/5
-
-
-        [HttpGet("{employeeId}/{password}")]
-        public ActionResult<Employee> GetByEmployeeId(string employeeId, string password)
-        {
-            var employee = employeeService.GetByeEmployeeId(employeeId);
-            if (employee == null)
-            {
-                return NotFound($"Employee with Id = {employeeId} not found");
-            }
-            if (employee.Password == password)
-            {
-                return Ok($"Employee with Id = {employeeId} logged in");
-            }
-            if (employee.Password != password)
-            {
-                return Ok($"Hey!! {employeeId} you entered Invalid Password");
-            }
-            else
-            {
-                return NotFound($"Employee with Id = {employeeId} not found");
-            }
-        }
-        
-
-
-
-
-
+     
     }
 }
